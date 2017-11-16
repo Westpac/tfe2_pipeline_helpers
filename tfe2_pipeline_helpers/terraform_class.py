9dabe3d515ab3c2b1241aeee8d9284233add6dab
Rory Chatterton <rchatterton@westpac.com.au>
2017-11-16 11:47:59 +1100

    Began adding unit tests

--- tfe2_pipeline_helpers/terraform_class.py
+++ tfe2_pipeline_helpers/terraform_class.py
@@ -4,53 +4,46 @@ import time
 import hcl
 import requests
 
+class TE22Connectivity:
+    def __init__(self, organisation, atlas_token, base_url="https://atlas.hashicorp.com/api/v2"):
 
-class TerraformAPICalls:
-    base_url = "https://atlas.hashicorp.com/api/v2"
-
-    def __init__(self, organisation, app_id, component_name, workspace_name, environment, repository, secrets,
-                 base_api_url=None):
-        self.header = {
-            'Authorization': "Bearer " + secrets["atlas_token"],
+        self.request_header = {
+            'Authorization': "Bearer " + atlas_token,
             'Content-Type': 'application/vnd.api+json'
         }
 
+        self.organisation = organisation()
+        self.base_url = base_url
+
+    def get(self, path, params=None):
+        return requests.get(url=self.base_url + path, headers=self.request_header, params=params)
+
+    def post(self, path, data, params=None):
+        return requests.post(url=self.base_url + path, data=data, headers=self.request_header, params=params)
+
+    def patch(self, path, data, params=None):
+        return requests.patch(url=self.base_url + path, data=data, headers=self.request_header, params=params)
+
+    def delete(self, path, params=None):
+        return requests.delete(url=self.base_url + path, headers=self.request_header, params=params)
+
+
+class TE2Runs:
+
+    def __init__(self, organisation, app_id, component_name, workspace_name, environment, repository, secrets,
+                 base_api_url=None):
+
+        self.te2_calls = TE22Connectivity(
+            organisation=organisation,
+            atlas_token=secrets['atlas_token']
+        )
+
         self.app_id = app_id
+        self.organisation = organisation
         self.environment = environment
         self.component_name = component_name
-        self.organisation = organisation
-        self.secrets = secrets
         self.workspace_name = workspace_name
         self.repository = repository
-
-    @staticmethod
-    def _render_request_data_workplace_variable_attributes(key, value, category, sensitive, hcl=False):
-        request_data = {
-            "data": {
-                "type": "vars",
-                "attributes": {
-                    "key": key,
-                    "value": value,
-                    "category": category,
-                    "sensitive": sensitive
-                }
-            }
-        }
-
-        if hcl:
-            request_data['data']['attributes']['hcl'] = True
-
-        return request_data
-
-    def _render_request_data_workplace_filter(self):
-        return {
-            "organization": {
-                "username": self.organisation
-            },
-            "workspace": {
-                "name": self.workspace_name
-            }
-        }
 
     def _render_request_run(self, destroy=False):
         return {
@@ -62,7 +55,7 @@ class TerraformAPICalls:
                     "workspace": {
                         "data": {
                             "type": "workspaces",
-                            "id": self.get_workspace_id
+                            "id": self._get_workspace_id()
                         }
                     }
                 },
@@ -71,94 +64,43 @@ class TerraformAPICalls:
         }
 
     def _get_workspace_id(self):
-        linkable_repo_url = self.base_url + "/organizations/" + self.organisation + "/workspaces"
-        response = requests.get(url=linkable_repo_url, headers=self.header)
+        response = self.te2_calls.get(path= "/organizations/" + self.organisation + "/workspaces")
 
         # Find the ID for the Repository that matches the repository name.
         for obj in response.json()['data']:
             if obj["attributes"]["name"] == self.workspace_name:
                 return obj["id"]
 
-    def _delete_all_variables(self):
-        request_uri = self.base_url + "/vars?filter[organization][username]=" + self.organisation + "&filter[workspace][name]=" + self.workspace_name
-
-        data = requests.get(request_uri, headers=self.header).text
-
-        # Delete Variables
-        for tfvar in json.loads(data)["data"]:
-            self.delete_variable(tfvar["id"])
-
-    def add_or_update_workspace_variable(self, key, value, category="terraform", sensitive=False,
-                                         hcl=False, variable_id=None):
-
-        request_url = self.base_url + "/vars"
-        request_data = self._render_request_data_workplace_variable_attributes(key, value, category, sensitive, hcl)
-
-        if variable_id:
-            request_data["data"]["id"] = variable_id
-            return requests.patch(url=request_url + "/" + variable_id, data=json.dumps(data), headers=self.header)
-
+        # Else return an empty object
         else:
-            request_data["filter"] = self._render_request_data_workplace_filter()
-            return requests.post(url=request_url, data=json.dumps(request_data), headers=self.header).status_code
+            return None
 
-    def get_workspace_variables(self):
-        request_url = self.base_url + "/vars/?filter[organization][username]" + self.organisation + \
-                      "?filter[workspace][name]" + self.workspace_name
-
-        return json.loads(requests.get(url=request_url, headers=self.header).text)
-
-    def load_secrets(self, destroy=False):
-        if "environment_variables" in self.secrets:
-            for obj in self.secrets["environment_variables"]:
-                self.add_workspace_variable(obj, self.secrets["environment_variables"][obj], category="env", hcl=False,
-                                            sensitive=True)
-
-        if "workspace_variables" in self.secrets:
-            for obj in self.secrets["workspace_variables"]:
-                self.add_workspace_variable(obj, self.secrets["workspace_variables"][obj], category="env", hcl=False,
-                                            sensitive=True)
-
-        if destroy:
-            self.add_workspace_variable("CONFIRM_DESTROY", "1", category="env", hcl=False,
-                                        sensitive=True)
-
+    # TODO: Rewrite the app variables.
     def load_app_variables(self, directory):
         url = "https://raw.githubusercontent.com/" + self.repository + "/env/" + self.environment + "/env/" + self.environment + ".tfvars"
 
         print("Getting Environment Variables from: " + url)
-        variable_list = hcl.loads(requests.get(url).text)
+        variable_list = hcl.loads(requests.get(url)).json()
         for obj in variable_list:
-            self.add_workspace_variable(obj, hcl.dumps(variable_list[obj]), hcl=True)
+            self._add_or_update_workspace_variable(obj, hcl.dumps(variable_list[obj]), hcl=True)
 
-        self.add_workspace_variable("app_id", self.app_id, hcl=False)
+        self._add_or_update_workspace_variable("app_id", self.app_id, hcl=False)
 
-    # Environment Variables from file
-    def load_environment_variables(self, directory):
-        with open(directory + "environment_variables.json", 'r') as fp:
-            variable_list = json.load(fp)
-            for obj in variable_list:
-                self.add_workspace_variable(obj, variable_list[obj], category="env",
-                                            sensitive=True)
-
-    def delete_variable(self, variable_id):
-        request_uri = self.base_url + "/vars/" + variable_id
-        return requests.delete(request_uri, headers=self.header)
-
-    def get_run_status(self, run_id="run-jDDckmxjRA1rTXVx"):
-        request_uri = self.base_url + "/runs/" + run_id
-        data = json.loads(requests.get(request_uri, headers=self.header).text)
+    # TODO: Error Handling
+    def get_run_status(self, run_id):
+        data = self.te2_calls.get("/runs/" + run_id).json()
         return data['data']['attributes']['status']
 
     def discard_untriggered_plans(self):
-        request_uri = self.base_url + "/workspaces/" + self.get_workspace_id() + "/runs"
-        # Get Status of all pending plans
 
+        # Get Status of all pending plans
         print("Discarding Untriggered Jobs")
 
         nothing_to_discard = False
         while not nothing_to_discard:
-            data = json.loads(requests.get(request_uri, headers=self.header).text)
+            data = self.te2_calls.get(
+                path="/workspaces/" + self._get_workspace_id() + "/runs"
+            ).json()
 
             nothing_to_discard = True
             for obj in data['data']:
@@ -172,28 +114,29 @@ class TerraformAPICalls:
                 elif obj["attributes"]["status"] == "pending" or obj["attributes"]["status"] == "planning":
                     nothing_to_discard = False
 
+    # TODO: Error Handling
     def discard_plan(self, run_id):
-        request_uri = self.base_url + "/runs/" + run_id + "/actions/discard"
-        data = {"comment": "Dropped by Jenkins Build"}
+        request_uri = "/runs/" + run_id + "/actions/discard"
+        data = {"comment": "Dropped by Jenkins Build"} # TODO: Add Job Number
 
-        return requests.post(request_uri, headers=self.header, data=json.dumps(data)).text
+        return self.te2_calls.post(
+            path="/runs/" + run_id + "/actions/discard",
+            data=json.dumps(data)
+        ).text
 
     def create_run(self, destroy=False):
 
         # Untriggered plans must be discarded before creating a new one is queued.
         self.discard_untriggered_plans()
-        self.delete_all_variables()
+        self._delete_all_variables()
         self.load_secrets(destroy)
         self.load_app_variables("")
 
-        request_uri = self.base_url + "/runs"
-        request_data = self._render_request_run(destroy)
-
-        return_data = requests.post(request_uri, headers=self.header, data=json.dumps(request_data))
+        return_data = self.te2_calls.post(path="/runs", data=self._render_request_run(destroy))
 
         print("Creating new Terraform run against: " + self.get_workspace_id)
 
-        self.delete_all_variables()
+        self._delete_all_variables()
 
         # Check if run can be created Successfully
         if str(return_data.status_code).startswith("2"):
@@ -208,10 +151,7 @@ class TerraformAPICalls:
                 print("Job Status: Planning")
                 time.sleep(5)
 
-                request = json.loads(requests.get(
-                    self.base_url + "/runs/" + json.loads(return_data.text)['data']['id'],
-                    headers=self.header
-                ).text)
+                request = self.te2_calls.get(path="/runs/" + return_data.text['data']['id']).json()
 
                 status = request['data']['attributes']['status']
                 changes_detected = request['data']['attributes']['has-changes']
@@ -261,12 +201,11 @@ class TerraformAPICalls:
         data = self._render_request_run(destroy)
 
         print("Applying Job: " + run_id)
-        print("Print URI: " + request_uri)
-        print(data)
 
-        return_data = requests.post(request_uri, headers=self.header, data=json.dumps(data))
+        return_data = self.te2_calls.post(path="/runs/" + run_id + "/actions/apply", data=json.dumps(data))
         print(return_data.status_code)
-        # Url endpoint not available
+
+        # TODO: Add link to logs
         # log_read_url = json.loads(return_data.text)['data']['attributes']['log-read-url']
 
         if str(return_data.status_code).startswith("2"):
@@ -277,7 +216,7 @@ class TerraformAPICalls:
                 print("Job Status: Applying changes")
                 time.sleep(5)
 
-                request = json.loads(requests.get(self.base_url + "/runs/" + run_id, headers=self.header).text)
+                request = self.te2_calls.get(path="/runs/" + run_id).json()
 
                 status = request['data']['attributes']['status']
 
@@ -285,7 +224,7 @@ class TerraformAPICalls:
             # print("Log File Directory:" + log_read_url)
             # print(requests.get(log_read_url, headers=self.header).text)
 
-            self.delete_all_variables()
+            self._delete_all_variables()
 
             # If Plan Failed
             if status == "errored":
@@ -304,3 +243,103 @@ class TerraformAPICalls:
             with open('data.json', 'w') as f:
                 json.dump({"status": "errored"}, f,
                           ensure_ascii=False)
+
+
+class TE2Variables():
+    def __init__(self, tfe2_calls, workspace_name, organisation, secrets):
+        self.te2_calls = tfe2_calls   # Connectivity class to provide function calls.
+        self.organisation = organisation
+        self.workspace_name = workspace_name
+        self.secrets = secrets
+
+    @staticmethod
+    def _render_request_data_workplace_variable_attributes(key, value, category, sensitive, hcl=False):
+        request_data = {
+            "data": {
+                "type": "vars",
+                "attributes": {
+                    "key": key,
+                    "value": value,
+                    "category": category,
+                    "sensitive": sensitive
+                }
+            }
+        }
+
+        if hcl:
+            request_data['data']['attributes']['hcl'] = True
+
+        return request_data
+
+    def _render_request_data_workplace_filter(self):
+        return {
+            "organization": {
+                "username": self.organisation
+            },
+            "workspace": {
+                "name": self.workspace_name
+            }
+        }
+
+    def _delete_variable(self, variable_id):
+        return self.te2_calls.delete(path="/vars/" + variable_id)
+
+    def _delete_all_variables(self):
+        params = {"filter[organization][username]": self.organisation,
+                  "filter[workspace][name]": self.workspace_name
+                  }
+
+        variable_list = self.te2_calls.get(path= "/vars", params=params).json()
+
+        # Delete Variables
+        for variable in variable_list["data"]:
+            self._delete_variable(variable["id"])
+
+    def get_workspace_variables(self):
+        params = {
+            "filter[organization][username]": self.organisation,
+            "filter[workspace][name]": self.workspace_name
+        }
+
+        return self.te2_calls.get(path="/vars", params=params).json()
+
+    # TODO: Error Handling
+    def _add_or_update_workspace_variable(self, key, value, category="terraform", sensitive=False,
+                                         hcl=False, variable_id=None):
+
+        request_data = self._render_request_data_workplace_variable_attributes(key, value, category, sensitive, hcl).json()
+
+        if variable_id:
+            request_data["data"]["id"] = variable_id
+            return self.te2_calls.patch(
+                path= "/vars/" + variable_id,
+                data=json.dumps(request_data)
+            )
+
+        else:
+            request_data["filter"] = self._render_request_data_workplace_filter()
+            return self.te2_calls.post(path="/vars", data=json.dumps(request_data)).status_code
+
+    def load_secrets(self, destroy=False):
+        if "environment_variables" in self.secrets:
+            for obj in self.secrets["environment_variables"]:
+                self._add_or_update_workspace_variable(obj, self.secrets["environment_variables"][obj], category="env", hcl=False,
+                                            sensitive=True)
+
+        if "workspace_variables" in self.secrets:
+            for obj in self.secrets["workspace_variables"]:
+                self._add_or_update_workspace_variable(obj, self.secrets["workspace_variables"][obj], category="env", hcl=False,
+                                            sensitive=True)
+
+        if destroy:
+            self._add_or_update_workspace_variable("CONFIRM_DESTROY", "1", category="env", hcl=False,
+                                        sensitive=True)
+
+
+    # Environment Variables from file
+    def load_environment_variables(self, directory):
+        with open(directory + "environment_variables.json", 'r') as fp:
+            variable_list = json.load(fp)
+            for obj in variable_list:
+                self._add_or_update_workspace_variable(obj, variable_list[obj], category="env",
+                                            sensitive=True)
